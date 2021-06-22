884b423ae9e669483e399e2c2ba3a27144f9270f
Jitendra <jitupmca@gmail.com>
2021-06-22 13:41:16 +0530

    removed lines from file

--- app2.py
+++ app2.py
@@ -18,9 +18,5 @@ def jitendra():
 def tushar_new():
     return render_template('tushar_new.html')
 
-@app.route("/surya_new.html")
-def surya_new():
-    return render_template('surya_new.html')
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=4080)
